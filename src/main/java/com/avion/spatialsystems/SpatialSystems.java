95991fca0d73f41b6ac5ff8bebc665ae3d23731a
Gabriel Tofvesson <gabriel.tofvesson@hotmail.com>
2017-04-15 11:32:37 +0200

    Refactored

---
+++ src/main/java/com/avion/spatialsystems/SpatialSystems.java
@@ -0,0 +1,18 @@
+package com.avion.spatialsystems;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = SpatialSystems.MODID, version = SpatialSystems.VERSION)
+public class SpatialSystems
+{
+    public static final String MODID = "spatialsystems";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
